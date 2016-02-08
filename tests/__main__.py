9638c47df9533d0fb88cbaa41350217d344efef9
Alexander Shorin <kxepal@gmail.com>
2016-02-08 09:36:23 +0300

    Move tests into package

---
+++ tests/__main__.py
@@ -0,0 +1,25 @@
+import inspect
+import unittest
+
+
+def discover_tests(mod):
+    for key, item in inspect.getmembers(mod):
+        if inspect.isclass(item) and issubclass(item, unittest.TestCase):
+            yield unittest.makeSuite(item, 'test')
+
+
+def common_tests():
+    from . import test_common
+    suite = unittest.TestSuite()
+    for test in discover_tests(test_common):
+        suite.addTest(test)
+    return suite
+
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(common_tests())
+    return suite
+
+
+unittest.main(defaultTest='suite')
